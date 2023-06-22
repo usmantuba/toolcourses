eb77647287019b490038a4742d26fd8350222a95
usmantuba <137394625+usmantuba@users.noreply.github.com>
2023-06-22 14:48:10 +0200

    Add files via upload

---
+++ ipynb.py
@@ -0,0 +1,112 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystem
+
+# ## In this notebook, Data Science Tools and Ecosystem are summarized.
+
+# **Objectives**
+# List popular languages for Data Science
+# + Github
+# + R
+# + Jupyter notebook
+# + SQL
+# + Data Science tool kits
+# 
+
+# **Objectives**
+# List popular languages for Data Science
+# + Github
+# + R
+# + Jupyter notebook
+# + SQL
+# + Data Science tool kits
+
+# In[ ]:
+
+
+### Some of the popular languages that Data Scientists use are:
+1. Java
+2. Python
+3. R
+
+
+# ### Some of the popular languages that Data Scientists use are:
+# 1. Java
+# 2. Python
+# 3. R
+
+# In[ ]:
+
+
+### Some of the commonly used libraries used by Data Scientists include:
+#### ordered list 
+1. numpy
+2. pandas
+3. breautifulsoup
+
+
+# ### Some of the commonly used libraries used by Data Scientists include:
+# #### ordered list 
+# 1. numpy
+# 2. pandas
+# 3. breautifulsoup
+
+# In[ ]:
+
+
+
+| Data Science Tools |
+| -------------------|
+| Github |
+| Python |
+| SQL |
+
+
+# 
+# | Data Science Tools |
+# | -------------------|
+# | Github |
+# | Python |
+# | SQL |
+
+# In[ ]:
+
+
+### Below are a few examples of evaluating arithmetic expressions in Python
+
+
+# ### Below are a few examples of evaluating arithmetic expressions in Python
+
+# In[ ]:
+
+
+(3*4)+5
+#This a simple arithmetic expression to mutiply then add integers
+
+
+# In[2]:
+
+
+(3*4)+5
+#This a simple arithmetic expression to mutiply then add integers
+
+
+# In[3]:
+
+
+(200/60)
+#This will convert 200 minutes to hours by diving by 60
+
+
+# ## Author 
+# Tuba
+
+# ## Author 
+# Tuba
+
+# In[ ]:
+
+
+
+
